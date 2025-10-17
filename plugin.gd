e7e34fa468d1117e788674964d225f28c6ca1ccb
Some Dev <somekindofgamedev@gmail.com>
2025-10-17 21:16:00 +0300

    Update plugin.gd

--- plugin.gd
+++ plugin.gd
@@ -5,11 +5,9 @@ const AUTOLOAD_NAME = "PolkaGodot"
 const AUTOLOAD_PATH = "res://addons/polkagodot/autoload/polkagodot.gd"
 
 func _enter_tree():
-	# Add the PolkaGodot singleton
 	add_autoload_singleton(AUTOLOAD_NAME, AUTOLOAD_PATH)
 	print("PolkaGodot plugin enabled")
 
 func _exit_tree():
-	# Remove the PolkaGodot singleton
 	remove_autoload_singleton(AUTOLOAD_NAME)
 	print("PolkaGodot plugin disabled")
