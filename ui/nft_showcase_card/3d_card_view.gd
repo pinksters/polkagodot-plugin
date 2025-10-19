a9c68161f921db2a48b5a2ee89a067968cfc5931
Some Dev <somekindofgamedev@gmail.com>
2025-10-19 17:33:35 +0300

    Smooth interpolation of 3D card movement

--- ui/nft_showcase_card/3d_card_view.gd
+++ ui/nft_showcase_card/3d_card_view.gd
@@ -5,9 +5,13 @@ extends Control
 @onready var mesh: MeshInstance3D = card.get_card_mesh()
 @onready var card_content: Control = card.get_card_content()
 
+var mouse_offset_target = Vector2(0, 0)
+var card_rotation_target = Vector2(0, 0)
+
+
 
 func _process(delta: float) -> void:
-	_align_card_with_mouse_offset()
+	_lerp_card_rotation(delta)
 
 
 func _align_card_with_mouse_offset() -> void:
@@ -17,3 +21,18 @@ func _align_card_with_mouse_offset() -> void:
 	mesh.rotation.x = mouse_offset.y * offset_scale
 	mesh.rotation.y = mouse_offset.x * offset_scale
 	card_content.update_parallax(mouse_offset.x, mouse_offset.y)
+
+
+func _lerp_card_rotation(delta: float) -> void:
+	const OFFSET_SCALE: float = 0.002
+	const LERP_SPEED: float = 10.0
+	
+	var max_mouse_offset: float = 370.0
+	var offset_bounds_origin: Vector2 = Vector2(0, -230)
+	var mouse_offset: Vector2 = center_marker.get_local_mouse_position()
+	if (mouse_offset - offset_bounds_origin).length() > max_mouse_offset: mouse_offset = Vector2.ZERO
+	
+	mouse_offset_target = lerp(mouse_offset_target, mouse_offset, delta * LERP_SPEED)
+	mesh.rotation.x = mouse_offset_target.y * OFFSET_SCALE
+	mesh.rotation.y = mouse_offset_target.x * OFFSET_SCALE
+	card_content.update_parallax(mouse_offset_target.x, mouse_offset_target.y)
